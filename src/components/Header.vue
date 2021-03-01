5cc6db38f5d6f50a376a987dcf1da8e9f20f18ad
Marcio Willian <willian_mw.cc@hotmail.com>
2021-03-01 04:36:25 -0300

    Implementando o botão de 'Comprar' ascações

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,23 @@
 <template>
   <v-card>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase mr-4">
+    <v-app-bar app>
+      <v-app-bar-title class="headline text-uppercase mr-4">
         <span>Stock</span>
         <span class="font-weight-light">Trader</span>
-      </v-toolbar-title>
-      <v-toolbar-items>
-        <v-btn flat to="/">Início</v-btn>
-        <v-btn flat to="/portfolio">Portfólio</v-btn>
-        <v-btn flat to="/stocks">Ações</v-btn>
-      </v-toolbar-items>
+      </v-app-bar-title>
+      <v-app-bar-items>
+        <v-btn text to="/">Início</v-btn>
+        <v-btn text to="/portfolio">Portfólio</v-btn>
+        <v-btn text to="/stocks">Ações</v-btn>
+      </v-app-bar-items>
 
       <v-spacer></v-spacer>
 
-      <v-toolbar-items>
-        <v-btn flat>Finalizar o Dia</v-btn>
+      <v-app-bar-items>
+        <v-btn text>Finalizar o Dia</v-btn>
         <v-menu offset-y>
           <template v-slot:activator="{ on, attrs }">
-            <v-btn flat v-bind="attrs" v-on="on"> Salvar & Carregar </v-btn>
+            <v-btn text v-bind="attrs" v-on="on"> Salvar & Carregar </v-btn>
           </template>
           <v-list>
             <v-list-item>
@@ -28,8 +28,8 @@
             </v-list-item>
           </v-list>
         </v-menu>
-      </v-toolbar-items>
-    </v-toolbar>
+      </v-app-bar-items>
+    </v-app-bar>
   </v-card>
 </template>
 
