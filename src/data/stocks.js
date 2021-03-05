cb25aea1460ce5fece7b655d74055fa3811fe5fd
Marcio Willian <willian_mw.cc@hotmail.com>
2021-03-05 05:36:42 -0300

    Adicionando mais duas ações

--- src/data/stocks.js
+++ src/data/stocks.js
@@ -2,5 +2,7 @@ export default [
     { id: 1, name: 'BMW',  price: 110 },
     { id: 2, name: 'Google',  price: 200 },
     { id: 3, name: 'Apple',  price: 280 },
-    { id: 4, name: 'Twitter',  price: 83 },
+    { id: 5, name: 'Twitter',  price: 83 },
+    { id: 6, name: 'Maggazine Luíza',  price: 180 },
+    { id: 7, name: 'Grupo Mateus',  price: 100 },
 ]
